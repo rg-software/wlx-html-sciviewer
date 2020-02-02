d1aa57e0c3185f9b9e85ae43a3c3dbfd7ce1d51b
Maxim Mozgovoy <rgsoftware@gmail.com>
2020-02-02 22:37:06 +0900

    build script created

---
+++ BuildMakeSetup.bat
@@ -0,0 +1,15 @@
+:: This script creates a release (setup) package using a prebuilt project.
+@echo off
+setlocal
+
+call "%VCINSTALLDIR%\Auxiliary\Build\vcvarsall.bat" x86
+msbuild sciviewer.sln /t:Build /p:Configuration=Release;Platform=Win32;UseEnv=true
+
+call "%VCINSTALLDIR%\Auxiliary\Build\vcvarsall.bat" x64
+msbuild sciviewer.sln /t:Build /p:Configuration=Release;Platform=x64;UseEnv=true
+
+del /Q plugin\sciviewer.zip
+copy /y README.md plugin\
+
+powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('plugin', 'sciviewer.zip'); }"
+echo Resulting file sciviewer.zip created!
